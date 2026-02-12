fd80af57d67ef304702ad3c22f00b5972d93ded8
zionfinance <zionfinanceconsultant@yahoo.com>
2026-02-12 15:28:20 +0200

    Create migration for payrolls table

---
+++ database/migrations/2026_02_12_000004_create_payrolls_table.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+
+class CreatePayrollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->decimal('amount', 10, 2);
+            $table->date('pay_date');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payrolls');
+    }
+}
